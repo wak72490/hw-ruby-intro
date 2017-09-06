c37890f30e2f9436fd2554cc8a3e00924e08fd6e
William Kennedy <wkenned4@citadel.edu>
2017-09-06 13:26:07 +0000

    finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -39,19 +39,60 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, "+ name
+  
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  if /[*aeiouAEIOU]/.match(s[0])
+    return false
+    
+  elsif s == nil  
+    return false
+    
+  elsif /0-9*/.match(s[0])
+    return false
+    
+  elsif /^$/.match(s)
+    return false
+    
+  elsif /[^a-zA-Z*]/.match(s)
+    return false
+    
+  else
+    return true
+ 
+  end 
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+ if s =~ /^[0-1]+$/
+   return s.to_i(2) % 4 == 0 
+ end
+ false
+  
 end
 
 # Part 3
 
 class BookInStock
+  
+attr_accessor :isbn
+attr_accessor :price
+
+def initialize (isbn, price)
+  
+  raise ArgumentError if isbn.empty? || price <= 0
+  
+  @isbn = isbn
+  @price = price
+  
+end
+
+def price_as_string
+  "$%0.2f" %  @price
+end
+  
+
 # YOUR CODE HERE
 end
